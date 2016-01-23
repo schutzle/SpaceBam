3c119c5d985841dc96568f73377dcdfa7d07ee1b
Marcel Schütz <m.schuetz@haptdikdesign.com>
2016-01-23 19:32:38 +0100

    add ship and level let the level scroll down

---
+++ Assets/Scripts/LevelMover.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelMover : MonoBehaviour {
+
+	public float speed;
+
+	void Start() {
+		GetComponent<Rigidbody>().velocity = -transform.forward * speed;
+	}
+}
