091d2433fe88141601888aa863c1ec7747537e97
m.schuetz <m.schuetz@haptikdesign.com>
2016-01-25 17:58:30 +0100

    add spawning script to spawner

---
+++ Assets/Scripts/SpawnOnContact.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnOnContact : MonoBehaviour {
+
+	public GameObject[] enemies;
+	public float spawnWait;
+	public float startWait;
+	public Transform spawnPosition;
+
+	void OnTriggerEnter (Collider other) {
+		if (other.CompareTag ("Boundary")) {
+			StartCoroutine (SpawnEnemies (transform));
+		}
+	}
+
+	IEnumerator SpawnEnemies (Transform transform)
+	{
+		yield return new WaitForSeconds (startWait);
+		foreach (GameObject enemy in enemies) {
+			Instantiate (enemy, transform.position, transform.rotation);
+			yield return new WaitForSeconds (spawnWait);
+		}
+	}
+}
